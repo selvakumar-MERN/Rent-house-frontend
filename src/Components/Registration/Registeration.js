dbb245da6f5c2428919da0a3ac1fb49847ec43be
selvakumar k <84513538+selvakumar-MERN@users.noreply.github.com>
2023-11-10 12:40:59 +0530

    Update Registeration.js

--- src/Components/Registration/Registeration.js
+++ src/Components/Registration/Registeration.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState } from 'react';
 import '../Login/Login.css'
 import { Link } from 'react-router-dom';
 import axios from 'axios';
